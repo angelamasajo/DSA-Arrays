875968e56961e7bd6fda9ffe9e7ce84f2809c6cb
Angela Masajo <aabmasajo@gmail.com>
2021-04-12 18:53:47 -0500

    largestSubarraySum

--- Assignment/maxSum.js
+++ Assignment/maxSum.js
@@ -1,36 +1,13 @@
-// function maxSum(array) {
-//   let maxSum = 0
-//   let sum = 0;
-//   for (let i = 0; i < array.length; i++) {
-//     sum = 0
-//     sum = array[i]
-//     for (let j = i+1; j < array.length; j++) {
-//       sum +- array[j]
-//       if (sum > maxSum) {
-//         maxSum = sum
-//       }
-//     }
-//   }
-//   return maxSum
-// }
 
-// console.log(maxSum([4, 6, -3, 5, -2, 1]))
+let largestSubarraySum = (array) => {
+  let currentSum = 0
+  let largestSum = 0
 
-
+  for(let number of array){
+      currentSum = Math.max(0, (currentSum + number))
+      largestSum = Math.max(largestSum, currentSum)
+  }
   
-// function maxSum(arr) {
-//   const sums = []
-//   arr.reduce((acc, curr) => {
-//       sums.push(acc + curr)
-//       return acc + curr
-//   })
-//   sums.sort((a, b) => a < b)    
-//   return sums[0]
-// }
+  return largestSum
 
-// const answer = maxSum([4, 6, -3, 5, -2, 1])
-// console.log(answer)
-
-
-
-//?????????????
+}
